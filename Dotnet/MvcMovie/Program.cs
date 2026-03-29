918a0b8c97eea7fd7e3fcc18dd89b43257db3972
DESKTOP-IRCBINQ\Losiu <loszegr7760_aehit@students.vizja.pl>
2026-03-29 15:07:53 +0200

    lab_3

--- Dotnet/MvcMovie/Program.cs
+++ Dotnet/MvcMovie/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
 using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddValidation();
 builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
